2a395ae2c6ad99d605d5991817f54423d44a2a1e
Yawen Chen <yawen214@gmail.com>
2015-02-17 20:10:08 -0600

    add main, random center gen

--- clustering.py
+++ clustering.py
@@ -24,3 +24,35 @@ def standardize(data):
         log_pts = [math.log(p+1) for p in pts]
         std_data.append((pts[0], log_pts))
     return std_data
+
+
+def random_centers(k_clusters, dimentions):
+''' Using random method to generate cluster center
+    Return a list of k centers with d dimentions
+'''
+    k = k_clusters
+    d = dimentions
+    clusters = []
+    random.seed(27945)      #seed the random
+    for i in range(k):
+        center = []
+        for i in range(d):  #d = 4 if given 4D data
+            r = random.random()
+            center.append(r)
+        clusters.append(center)
+    return clusters
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--clusters', type= int, required=True, help='Input the number of clusters')
+    parser.add_argument('-f', '--filename', required=True, help='Input file name')
+    #parser.add_argument('-m','-method', required = False, help = 'Choose the method for initializing the cluster centers')
+
+    args = parser.parse_args()
+    k = args.clusters
+    #if args.method = 2:
+        #print "Implimentation coming soon"      
+    cluster_centers = random_center(k, 4)
+
+if __name__ == '__main__':
+    main()
