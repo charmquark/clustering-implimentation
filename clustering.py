a132a9a2d23a40e9a6fdacb93b34ef2999de8057
Brian Charous <brian.charous@gmail.com>
2015-02-21 15:30:20 -0600

    works w/generating random centers among the points

--- clustering.py
+++ clustering.py
@@ -1,9 +1,22 @@
-# -*- coding: utf-8 -*- 
 import codecs
 import string
 import math
 import random
 import argparse
+import sys
+
+class Center(object):
+    """docstring for Center"""
+    def __init__(self, location):
+        super(Center, self).__init__()
+        self.location = location
+        self.points = []
+
+    def __repr__(self):
+        # undo log_2(x+1)
+        unlogged_loc = [2**(i-1) for i in self.location]
+        return "<Center> at {0}".format(self.location)
+
 
 def read_data(filename):
     """ return list of points from file with structure like
@@ -19,53 +32,26 @@ def read_data(filename):
             data.append((name, coords))
     return data
 
-def clean_data(data):
-    """ put the given data into dictionary with key: editors' name and value: the points
-        assuming given data is in the format [('haa',[1,2,3,5....]), ...)]
-        return a list conists of a dictionary and data withouth editor username
-     """
-    data_dict={}
-    clean_data = []
-    for item in data:
-        data_dict[item[0]] = item[1]
-        clean_data.append(item[1])
-    return [clean_data, data_dict]
-
 def standardize(data):
     """ convert every value x in data to log(x+1) """
     std_data = []
     for entry in data:
         pts = entry[1]
-        log_pts = [math.log(p+1) for p in pts]
-        std_data.append((pts[0], log_pts))
+        log_pts = [math.log(p+1)/math.log(2) for p in pts]
+        std_data.append((entry[0], log_pts))
     return std_data
-
-def random_centers_from_limit(k_clusters, dimentions, limit):
-    """Do not use this for now. need limit, which is the range we get from data"""
-    k = k_clusters
-    d = dimentions
-    centers = []
-    random.seed(27945)      # seed the random
-    for i in range(k):
-        center = []
-        for i in range(d):   #d = 4 if given 4D data
-            r = random.random()
-            center.append(r * limit)
-        centers.append(center)
-    return centers
 
 def random_centers_from_data(k, data):
     """ generate random centers with given data 
         assuming given data is in the format [('haa',[1,2,3,5....]), ...)]
     """
     index = len(data)
-    #random.seed(78902)     # seed is not used
-    centers =[] 
+    random.seed(123)
+    centers = [] 
     for i in range(k): # number of ranges
         rand = random.randint(1, index)    # generate random number in the range of 1 to the length of the data
-        center =[]
-        for j in range(len(data[0][1])): # the dimension of the data      
-            center.append(data[rand-1][1][j])
+        point = data[rand]
+        center = Center(point[1])
         centers.append(center)
     return centers
 
@@ -73,96 +59,101 @@ def gen_new_centers(clusters):
     """ return a list of new cluster centers 
         given the current clusters dictionary (currently without their id)
     """
-    centers =[]
+    centers = []
     for key, values in clusters.iteritems():
         num_points = len(values)     # number of points in data
         d = len(values[0])   #dimension of the data
         cluster_sum=[0] * d
         for i in range(num_points):     # each point within the cluster
-            for j in range(d):                   # each dimension of each point
+            for j in range(d):              # each dimension of each point
                 cluster_sum[j] += value[i][j]
         for each_sum in cluster_sum:
             centers.append[each_sum/num_points]
         centers.append(centers)
     return centers
 
-def gen_clusters (centers, data):
+def move_centers(centers):
+    """ given a list of centers, move each center based on where the average 
+    of the points assigned to it lies """
+    for center in centers:
+        avg = [0] * len(center.location)
+        for point in center.points:
+            for i, elem in enumerate(point[1]):
+                avg[i] += elem
+        if len(center.points) > 0:
+            avg = [i/len(center.points) for i in avg]
+            center.location = avg
+
+def gen_clusters(centers, data):
     """ returned data format, when k = 3: [([1,2,3,4],[2,2,3,4],[3,2,3,4],....),[],[]]"""
     k = len(centers)
-    result = [()] * k
-    dict = {}
-    for i, center in enumerate(centers):
-        dict[i] = center      
-        result[i]
     for point in data:       # number of points
         assignment = min_squared_euclidean_dist(centers, point[1])
-        index = get_key(dict, assignment)
-        result[index].append(point)
-    return result
-
-def get_key(dict, value):
-    """ helper function getting key fiven a value for a dictionary.
-        This is faster
-     """
-    for index, point in dict.iteritems():
-        if point == value:
-            return index
+        # print result[index]
+        assignment.points.append(point)
 
 def min_squared_euclidean_dist(centers, point):
     """ return the assignment for the point given several centers as choices"""
     min_dist = float("infinity")
-    cur_dist = 0
+    assignment = None
     for center in centers:
-        for i in range(len(center)):
-            cur_dist += (center[i] - point[i])**2
+        cur_dist = 0
+        for i in range(len(center.location)):
+            cur_dist += (center.location[i] - point[i])**2
         if cur_dist < min_dist:           
             min_dist = cur_dist
             assignment = center 
     return assignment
 
+def sse(centers):
+    """ compute sum of squares error for data """
+    sse = 0
+    for center in centers:
+        for point in center.points:
+            p = point[1]
+            for i in range(len(p)):
+                sse += (center.location[i] - p[i])**2
+    return sse
+
+def kmeans(data, k):
+    """ run k-means algorithm """
+    centers = random_centers_from_data(k, data)
+    gen_clusters(centers, data)
+    sse_val = float("infinity")
+    iter_num = 1
+    while True:
+        move_centers(centers)
+        all_points = []
+        for center in centers:
+            all_points.extend(center.points)
+            center.points = []
+        gen_clusters(centers, all_points)
+        new_sse = sse(centers)
+        print "SSE before iteration {0}: {1}, after: {2}".format(iter_num, sse_val, new_sse)
+        assert new_sse <= sse_val
+        if new_sse == sse_val:
+            return centers
+        sse_val = new_sse
+        iter_num += 1
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', '--clusters', type= int, required=True, help='Input the number of clusters')
     parser.add_argument('-f', '--filename', required=True, help='Input file name')
-    #parser.add_argument('-m','-method', required = False, help = 'Choose the method for initializing the cluster centers')
+    # parser.add_argument('-m','-method', required = False, help = 'Choose the method for initializing the cluster centers')
 
     args = parser.parse_args()
     k = args.clusters
     filename = args.filename
 
-    # do something with choosing a cluster centers initialization method
-
-    # the following are for debugging, eventually we gonna have one function to call from main 
-    # and run until it converge
-
-    # readfile 
-    data = read_data(filename)
-   # standadize data
-    data = standardize(data)
-
-    # clean data, not necessary
-    #data = process_data(data)
-    #data_clean = data[0]
-    #data_dict = data[1]
-
-    # generating initial centers from random points from data
-    centers = random_centers_from_data(k,data)  
-    # for test purpse, test initial center
-    for center in centers:
-        print "the initial random center is:{0}, \n".format(center)
-
-    # calculating and form some clusters with the centers
-    clusters = gen_clusters(centers, data)
-
-    # do sometthing with SSE
-
-    # generating the new centers
-    centers = gen_new_centers(clusters)
-    # assigne the each point to the closest new centers 
-
-
-
+    # read file 
+    data = standardize(read_data(filename))
+    clusters = kmeans(data, k)
+    for center in clusters:
+        print center
+        for point in center.points:
+            sys.stdout.write(point[0] + ', ')
+        print "\n"
 
 if __name__ == '__main__':
     main()
